14f244b05e4a2646ecdf09ee96bf4e0a279ef7ed
pablogch <pablochoconi@gmail.com>
2025-12-28 16:47:09 -0300

    Agregados tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -13,4 +13,22 @@ describe('App', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should render router-outlet', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled.innerHTML);
+    const routerOutlet = compiled.querySelector('router-outlet');
+    expect(routerOutlet).toBeTruthy();
+  });
+
+  it('should render router-outlet with css classes', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const divElement = compiled.querySelector('div');
+    const mostHaveClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+    divElement?.classList.forEach((className) => {
+      expect(mostHaveClasses).toContain(className);
+    })
+  });
 });
